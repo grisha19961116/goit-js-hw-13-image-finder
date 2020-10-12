8966ae5539267fe1d77b214bc9b90b825e431ddc
unknown <grisha1996111616@gmail.com>
2020-10-12 22:09:10 +0100

    some fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 
 import './styles.css';
-import  './java-script/renderGallery.js';
-// webformatURL - посилання на маленьке зображення для списку карток
-// largeImageURL - посилання на велике зображення (дивись пункт 'додатково')
-// likes - кількість лайків
-// views - кількість переглядів
-// comments - кількість коментарів
-// downloads - кількість завантажень
+import  './java-script/renderGallerytSetting';
+
 
 
 
